c36beab7c14057387fb3723fcb7fcae9248c0061
Karl Fritsche <karl.fritsche@cocomore.com>
2015-06-24 12:44:27 +0200

    Added possibility to use a own config loader (patched for 4.7.5)

--- src/TextUI/Command.php
+++ src/TextUI/Command.php
@@ -38,6 +38,7 @@ class PHPUnit_TextUI_Command
         'bootstrap='           => null,
         'columns='             => null,
         'configuration='       => null,
+        'loaderConfig='        => null,
         'coverage-clover='     => null,
         'coverage-crap4j='     => null,
         'coverage-html='       => null,
@@ -258,6 +259,10 @@ class PHPUnit_TextUI_Command
                 case 'c':
                 case '--configuration':
                     $this->arguments['configuration'] = $option[1];
+                    break;
+
+                case '--loaderConfig':
+                    $this->arguments['configurationClass'] = $option[1];
                     break;
 
                 case '--coverage-clover':
@@ -551,8 +556,10 @@ class PHPUnit_TextUI_Command
 
         if (isset($this->arguments['configuration'])) {
             try {
+                $configurationClass = isset($this->arguments['configurationClass']) ? $this->arguments['configurationClass'] : NULL;
                 $configuration = PHPUnit_Util_Configuration::getInstance(
-                    $this->arguments['configuration']
+                    $this->arguments['configuration'],
+                    $configurationClass
                 );
             } catch (Throwable $e) {
                 print $e->getMessage() . "\n";
